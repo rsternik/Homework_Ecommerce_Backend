eb4d91abd4751f4d509aac6b54bb5a01b3d6bde4
rsternik <rsternik.wg@gmail.com>
2022-01-28 17:00:15 -0800

    Initial commit of files, still working on model files

---
+++ models/Category.js
@@ -0,0 +1,35 @@
+// Required sequalize module
+const { Model, DataTypes } = require('sequelize');
+
+// Required connection file
+const sequelize = require('../config/connection.js');
+
+// Category creation 
+class Category extends Model {}
+
+// Category model for db column params
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+//export Category sequelize Model
+module.exports = Category;
